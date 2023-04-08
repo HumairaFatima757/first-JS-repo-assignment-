6f1c867f9d6efa5eedc22131a007ed3b135392e3
HumairaFatima757 <fatimahumaira757@gmail.com>
2023-04-08 13:03:35 +0500

    1st  js  ass commit

---
+++ app.js
@@ -0,0 +1,69 @@
+ 
+//  =========Q # 1 =====================
+ 
+//  alert("welcome to our website");
+
+// ============Q# 2 =====================
+//  alert(" JAVA SCRIPT Alert \n \n Error! Please enter a valid password")
+
+//  ====================Q# 3==========
+// alert(" Welcome to JS Land \n Happy Coding "     )
+
+// ================Q # 4 ===========
+// alert("javaScript alert  \n   Welcome to JS land  \n  ")
+// alert(" Happy Coding ...\n Prevent this page from creating additional dialogs")
+
+// ======================Q # 5 ==============
+
+// alert("javaScript alert \n \n Happy Coding ! \n Prevent this page from creating additional dialogs   ")
+
+// ============================Q# 6 =============
+// alert("JavaScript Alert \n \n Hello ..! I can run JS through my web Browser's console ")
+
+
+// ASSIGNMeNT #2=======================88888888888888888888888888888888========================
+// ==== q # 1 
+// var userName = "John Doe";
+// ===q # 2
+
+// var myName = "Humaira fatima ";
+
+
+// === q # 3
+// var message = "Hello World";
+// alert(message);
+
+
+// ===q # 4
+// var nam = "Jhon e Doe ";
+// var age  = "15 years old  ";
+// var skill = "Certified mobile application development";
+// alert(nam);
+// alert(age);
+// alert(skill);
+
+
+//====q # 5
+
+// var cut = "pizza \n pizz \n piz \n pi \n p"
+// alert(cut);
+
+
+//====q # 6
+// var email = "fatimahumaira@gmail.com";
+// alert("My email address is "+ email);
+
+
+//====q # 7
+// var book = "A Smarter way to learn javasript ";
+// alert(book)
+//====q # 8
+
+// var s = "Yah! i can write html content through javascript "
+// document.write(s);
+
+
+//====q # 9
+// var style = "       “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” \n \n "
+// alert(style );
+// document.write(style);
